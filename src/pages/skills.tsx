f393124c00c6e224559651f831f139ab5bcf889b
kzk4043 <okazakishoma@team-lab.com>
2023-06-12 16:56:46 +0900

    feat: skills i18n

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,4 +1,5 @@
 import { graphql, type HeadFC, type PageProps } from 'gatsby';
+import { Trans, useTranslation } from 'gatsby-plugin-react-i18next';
 import * as React from 'react';
 import {
   Radar,
@@ -13,39 +14,40 @@ import AppLink from '@/components/appLink';
 import Layout from '@/components/layout';
 
 const SkillsPage: React.FC<PageProps> = () => {
+  const { t } = useTranslation();
   const data = [
     {
-      subject: 'プロジェクトマネジメント',
+      subject: t('プロジェクトマネジメント'),
       score: 4,
     },
     {
-      subject: 'デザイン',
+      subject: t('デザイン'),
       score: 1,
     },
     {
-      subject: 'フロントエンド',
+      subject: t('フロントエンド'),
       score: 4,
     },
     {
-      subject: 'バックエンド',
+      subject: t('バックエンド'),
       score: 1,
     },
     {
-      subject: 'インフラ',
+      subject: t('インフラ'),
       score: 1,
     },
     {
-      subject: 'その他',
+      subject: t('その他'),
       score: 3,
     },
   ];
   const tickLevel = [
-    '未経験',
-    '個人開発、研修',
-    '駆け出し',
-    '中堅',
-    'プロジェクトリーダ',
-    'テックリード',
+    t('未経験'),
+    t('個人開発、研修'),
+    t('駆け出し'),
+    t('中堅'),
+    t('プロジェクトリーダ'),
+    t('テックリード'),
   ];
 
   return (
@@ -84,36 +86,76 @@ const SkillsPage: React.FC<PageProps> = () => {
           </ResponsiveContainer>
         </div>
         <div className="ml-14 mt-14 lg:ml-0 lg:mt-0 lg:px-5 lg:pb-5">
-          <h2 className="font-bold underline">プロジェクトマネジメント</h2>
-          <p className="mt-3">アジャイル/ウォーターフォール</p>
-          <p className="mt-2">
-            富士通時代はプロジェクトマネージャとして、SaaSのカスタマイズプロジェクト、およびそのシステムの保守部隊の構築を担当していました。開発/保守はインドのチームと、アジャイルでの開発でした。チームラボではウォーターフォールでの開発がメインとなります。
-          </p>
-          <h2 className="mt-8 font-bold underline">デザイン</h2>
+          <h2 className="font-bold underline">
+            <Trans>プロジェクトマネジメント</Trans>
+          </h2>
           <p className="mt-3">
-            実務経験はありませんが、このポートフォリオサイトのデザインをはじめ勉強中です。
+            <Trans>アジャイル/ウォーターフォール</Trans>
           </p>
-          <h2 className="mt-8 font-bold underline">フロントエンド</h2>
+          <p className="mt-2">
+            <Trans>
+              富士通時代はプロジェクトマネージャとして、SaaSのカスタマイズプロジェクト、およびそのシステムの保守部隊の構築を担当していました。開発/保守はインドのチームと、アジャイルでの開発でした。チームラボではウォーターフォールでの開発がメインとなります。
+            </Trans>
+          </p>
+          <h2 className="mt-8 font-bold underline">
+            <Trans>デザイン</Trans>
+          </h2>
+          <p className="mt-3">Figma</p>
+          <p className="mt-2">
+            <Trans>
+              実務経験はありませんが、このポートフォリオサイトのデザインをはじめ勉強中です。
+            </Trans>
+          </p>
+          <h2 className="mt-8 font-bold underline">
+            <Trans>フロントエンド</Trans>
+          </h2>
           <p className="mt-3">Next.js/Nuxt.js/TypeScript/HTML/CSS</p>
           <p className="mt-2">
-            チームラボではフロントエンド班に所属しており、私のメインスキルになります。Next.js/Nuxt.jsでの構築経験があり、中規模プロジェクトのフロントエンドリーダも経験させていただきました。
+            <Trans>
+              チームラボではフロントエンド班に所属しており、私のメインスキルになります。Next.js/Nuxt.jsでの構築経験があり、中規模プロジェクトのフロントエンドリーダも経験させていただきました。
+            </Trans>
           </p>
-          <h2 className="mt-8 font-bold underline">バックエンド</h2>
+          <h2 className="mt-8 font-bold underline">
+            <Trans>バックエンド</Trans>
+          </h2>
           <p className="mt-3">
-            実務経験はありませんが、
-            <AppLink to="https://praha-challenge.com">プラハチャレンジ</AppLink>
-            という中級エンジニア向けブートキャンプにて勉強中です。
+            <Trans
+              i18nKey="backend"
+              components={{
+                l: (
+                  <AppLink to="https://praha-challenge.com">
+                    プラハチャレンジ
+                  </AppLink>
+                ),
+              }}
+            />
           </p>
-          <h2 className="mt-8 font-bold underline">インフラ</h2>
-          <p className="mt-3">
-            実務経験はありませんが、
-            <AppLink to="https://praha-challenge.com">プラハチャレンジ</AppLink>
-            という中級エンジニア向けブートキャンプにて勉強中です。AWSクラウドプラクティショナー取得。
-          </p>
-          <h2 className="mt-8 font-bold underline">その他</h2>
-          <p className="mt-3">採用/Git/Github/Gitlab/Slack/GAS</p>
+          <h2 className="mt-8 font-bold underline">
+            <Trans>インフラ</Trans>
+          </h2>
+          <p className="mt-3">AWS</p>
           <p className="mt-2">
-            チームラボへの入社後採用にも関わらせていただいており、書類選考/面接から、フロントエンド班における採用ルールの整備なども担当させていただきました。業務効率化のため簡単なSlackアプリの導入なども行っています。
+            <Trans
+              i18nKey="infrastructure"
+              components={{
+                l: (
+                  <AppLink to="https://praha-challenge.com">
+                    プラハチャレンジ
+                  </AppLink>
+                ),
+              }}
+            />
+          </p>
+          <h2 className="mt-8 font-bold underline">
+            <Trans>その他</Trans>
+          </h2>
+          <p className="mt-3">
+            <Trans>採用/Git/Github/Gitlab/Slack/GAS</Trans>
+          </p>
+          <p className="mt-2">
+            <Trans>
+              チームラボへの入社後採用にも関わらせていただいており、書類選考/面接から、フロントエンド班における採用ルールの整備なども担当させていただきました。業務効率化のため簡単なSlackアプリの導入なども行っています。
+            </Trans>
           </p>
         </div>
       </div>
