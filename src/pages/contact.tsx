28768ba3166232003322f009b6b3b3d11230675c
kzk4043 <okazakishoma@team-lab.com>
2023-06-20 22:51:32 +0900

    feat: contact、メール送れるようにした

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,46 @@
+import emailjs from '@emailjs/browser';
 import { graphql, type HeadFC, type PageProps } from 'gatsby';
-import * as React from 'react';
+import React from 'react';
 import Layout from '@/components/layout';
 
 const ContactPage: React.FC<PageProps> = () => {
+  const form = React.useRef<HTMLFormElement>(null);
+  const [emailStatusMessage, setEmailStatusMessage] = React.useState('');
+
+  const sendEmail = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!form.current) return;
+
+    try {
+      await emailjs.sendForm(
+        'service_gajfc0u',
+        'template_0frletv',
+        form.current,
+        '2aJrhnLqd2u5p_3Yz'
+      );
+
+      setEmailStatusMessage('メールが送信されました！返信をお待ち下さい。');
+    } catch (error) {
+      setEmailStatusMessage(
+        'メール送信時にエラーが発生しました！お手数ですが再度送信してください。'
+      );
+    }
+  };
+
   return (
     <Layout>
-      <div className="flex h-[80vh] w-full items-center justify-center text-3xl">
-        <div>coming soon</div>
+      <div className="w-full">
+        <p>{emailStatusMessage}</p>
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
       </div>
     </Layout>
   );
