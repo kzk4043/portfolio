0417c01ff2a93f65f8259b6e9dfdd21dff3d19d9
kzk4043 <okazakishoma@team-lab.com>
2023-07-29 21:46:51 +0900

    feat: 初回およびページ遷移アニメーション追加

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,11 +1,7 @@
 import { useLayoutEffect, useState } from 'react';
 
 export const useWindowSize = (): number[] => {
-  const isClient = typeof window !== 'undefined';
-  /**
-   * 初期値のまま一旦レンダリングされてしまうので、クライアントサイドでは初期値をウィンドウサイズにする
-   */
-  const [size, setSize] = useState([isClient ? window.innerWidth : 0, isClient ? window.innerHeight : 0]);
+  const [size, setSize] = useState([0, 0]);
 
   useLayoutEffect(() => {
     const updateSize = (): void => {
